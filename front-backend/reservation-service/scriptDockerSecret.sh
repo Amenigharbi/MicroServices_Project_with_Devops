0a274c47c78565ddc33ca6cd47eb8b97b3b624c6
Amenigharbi <gharbiameni17@gmail.com>
2024-09-10 15:06:14 +0100

    update k8s manifests

--- front-backend/reservation-service/scriptDockerSecret.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-kubectl create secret docker-registry my-dockerhub-secret \
-  --docker-server=https://index.docker.io/v1/ \
-  --docker-username=$DOCKERHUB_USERNAME \
-  --docker-password=$DOCKERHUB_TOKEN \
-  --docker-email=chetouiiftikhar@gmail.com  # Optionnel, remplacez par votre email
-
-echo "Secret Docker Hub créé avec succès !"
